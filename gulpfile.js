8af8537ea508c4c5f0e0712e29cc8866b8557f08
Peter Burns <rictic@google.com>
2016-06-28 12:39:04 -0700

    Just use the defaults from tools-common

--- gulpfile.js
+++ gulpfile.js
@@ -10,76 +10,8 @@
 
 'use strict';
 
-const depcheck = require('depcheck');
-const eslint = require('gulp-eslint');
-const fs = require('fs-extra');
+const toolsCommon = require('tools-common/gulpfile');
 const gulp = require('gulp');
-const mergeStream = require('merge-stream');
-const mocha = require('gulp-mocha');
-const path = require('path');
-const runSeq = require('run-sequence');
-const tslint = require("gulp-tslint");
-const typescript = require('gulp-typescript');
-const typings = require('gulp-typings');
 
-const tsProject = typescript.createProject('tsconfig.json');
-
-gulp.task('init', () => gulp.src("./typings.json").pipe(typings()));
-
-gulp.task('lint', ['tslint', 'eslint', 'depcheck']);
-
-gulp.task('build', () =>
-  mergeStream(
-    gulp.src('src/**/*.ts').pipe(typescript(tsProject)),
-    gulp.src(['src/**/*', '!src/**/*.ts'])
-  ).pipe(gulp.dest('lib'))
-);
-
-gulp.task('clean', (done) => {
-  fs.remove(path.join(__dirname, 'lib'), done);
-});
-
-gulp.task('build-all', (done) => {
-  runSeq('clean', 'init', 'lint', 'build', done);
-});
-
-gulp.task('test', ['build'], () =>
-  gulp.src('test/**/*_test.js', {read: false})
-      .pipe(mocha({
-        ui: 'tdd',
-        reporter: 'spec',
-      }))
-);
-
-gulp.task('tslint', () =>
-  gulp.src('src/**/*.ts')
-    .pipe(tslint({
-      configuration: 'tslint.json',
-    }))
-    .pipe(tslint.report('verbose')));
-
-gulp.task('eslint', () =>
-  gulp.src(['test/**/*.js', 'scripts/**/*.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError()));
-
-gulp.task('depcheck', () => new Promise((resolve, reject) => {
-  depcheck(__dirname, {ignoreDirs: []}, (result) => {
-    let invalidFiles = Object.keys(result.invalidFiles) || [];
-    let invalidJsFiles = invalidFiles.filter((f) => f.endsWith('.js'));
-    if (invalidJsFiles.length > 0) {
-      console.log('Invalid files:', result.invalidFiles);
-      reject(new Error('Invalid files'));
-      return;
-    }
-
-    if (result.dependencies.length) {
-      console.log('Unused dependencies:', unused.dependencies);
-      reject(new Error('Unused dependencies'));
-      return;
-    }
-
-    resolve();
-  });
-}));
+// All the default values work fine here!
+toolsCommon.test(gulp);
